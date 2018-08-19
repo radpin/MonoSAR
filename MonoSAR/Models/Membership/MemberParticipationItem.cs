6e4f8f7211cbe56a04ffceac4ef0c9e03577d2db
Lisa Cowan <lisagitel@hotmail.com>
2018-08-18 17:49:56 -0700

    Added member participation report. It currently shows trainings and ops for this year and total.

---
+++ MonoSAR/Models/Membership/MemberParticipationItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonoSAR.Models.Membership
+{
+/// <summary>
+/// This transforms the EF model of "member", and the child properties and collections, into a model for MVC binding. The child objects (medical, certs, cpr, etc) are 
+/// have their transformations as part of their constructors. 
+/// </summary>
+    public class MemberParticipationItem
+    {
+        public MemberParticipationItem()
+        {
+        }
+
+        public Int32 ID { get; set; }
+        public String First { get; set; }
+        public String Last { get; set; }
+        public int NumOperationsThisYear { get; set; }
+        public int NumTrainingsThisYear { get; set; }
+        public int NumOperationsTotal { get; set; }
+        public int NumTrainingsTotal { get; set; }
+    }
+}
